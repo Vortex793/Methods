2990b7416fa3fb37365972f5befeb65e18703cfb
339821175 <339821175@2260I-L29-020>
2026-09-14 12:40:10 -0400

    done

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -8,33 +8,16 @@ namespace Methods
 {
     internal class Program
     {
-
+        //CURTIS APFELBECK
         public static void DrawGuitar()
         {
             Console.WriteLine("");
-
             Console.WriteLine("                  _,.,");
-
-            
             Console.WriteLine("               ,'   ,'");
-
-            
-
             Console.WriteLine("               /   ,'");
-
-            
-
             Console.WriteLine("              /   , ");
-
-            
-
             Console.WriteLine("             /   ,");
-
-           
-
             Console.WriteLine("            /   '");
-            
-
             Console.WriteLine("           /   ,'");
             Console.WriteLine("           '.__|");
             Console.WriteLine("            |  |");
@@ -69,9 +52,80 @@ namespace Methods
             Console.WriteLine("',,..--~~^");
             Console.WriteLine("");
         }
+
+        public static void DrawFace()
+        {
+            Console.WriteLine("____||____");
+            Console.WriteLine(" ///////////\\");
+            Console.WriteLine("///////////  \\");
+            Console.WriteLine("|    _    |  |");
+            Console.WriteLine("|[] | | []|[]|");
+            Console.WriteLine("|   | |   |  |");
+
+
+
+
+        }
+
+       public static void DrawYellowSubmarine()
+        {
+            Console.WriteLine("_.._");
+            Console.WriteLine("                                         (    )");
+            Console.WriteLine("                                     .--.\\   /");
+            Console.WriteLine("                                     ( )) \\_.-,");
+            Console.WriteLine("                                     _`\\  ( (( )");
+            Console.WriteLine("                                    ( ``|  |  |");
+            Console.WriteLine("                                     `|_|__|__|");
+            Console.WriteLine("                                     /_ __ _ __|");
+            Console.WriteLine("                                   .-L_7__7_7_7|");
+            Console.WriteLine("_______                           (   ,----,  (`.");
+            Console.WriteLine("\\\"\"\"\"\"\";`.                         |  .\\ \\ / /.|  \\");
+            Console.WriteLine("/    [|  \\                        |  :-.-\"-,-:|   `-------.");
+            Console.WriteLine("\\    [|   `-.___________________.-'  |_:_Q_:_|`-._______)  `----------.._");
+            Console.WriteLine("/    [|         (       /   |        \"\"\"\"\"\"\"\"\" .------------------' (__) `.");
+            Console.WriteLine("\\    [|          `-----'    |                 /  .--.  .--. .--.  .--.    ;");
+            Console.WriteLine("/_____;                     7----------------'  :    ;:    ;;   ;:    :  /");
+            Console.WriteLine("\"\"\"\"\"\"\"\"\"-.               .'   S$S S$S S$S S$S   `--'  `--' `--'  `--' .'");
+            Console.WriteLine("          ,)              |                            |\"\"\"\"\"\"\"\"|     (");
+            Console.WriteLine("          0-------....._  |             _________      |        |      )");
+            Console.WriteLine("          '             `.|           .\"         \".    |        |    .'");
+            Console.WriteLine("                          `-._        |           |    |________| .-'");
+            Console.WriteLine("                              `-..____|___________|_________...--'   ");
+        }
+        
+        public static void KnockKnock()
+        {
+            Console.WriteLine("Knock Knock");
+            string who = Console.ReadLine().ToLower();
+            if (who == "who's there" || who == "whos there")
+            {
+                Console.WriteLine("Yah");
+                string yah = Console.ReadLine().ToLower();
+                if (yah == "yah who?" || yah == "yah who" || yah == "yahoo" || yah == "yahwho")
+                {
+                    Console.WriteLine("No I use Google.");
+                }
+                else
+                {
+                    Console.WriteLine("You should say \"Yah who?\"");
+                    return;
+                }
+            }
+            else
+            {
+                Console.WriteLine("You should say \"Who's there?\"");
+                return;
+            }
+            
+
+
+        }
         static void Main(string[] args)
         {
-
+            DrawGuitar();
+            DrawFace();
+            DrawYellowSubmarine();
+            KnockKnock();
         }
     }
 }
