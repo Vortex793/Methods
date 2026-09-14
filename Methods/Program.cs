1350cec79c14cf9b95e149d4076890730aa1a8ce
Vortex793 <Curtisapf94@gmail.com>
2026-09-14 09:02:09 -0400

    Add project files.

---
+++ Methods/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    internal class Program
+    {
+
+        public static void DrawGuitar()
+        {
+            Console.WriteLine("");
+
+            Console.WriteLine("                  _,.,");
+
+            
+            Console.WriteLine("               ,'   ,'");
+
+            
+
+            Console.WriteLine("               /   ,'");
+
+            
+
+            Console.WriteLine("              /   , ");
+
+            
+
+            Console.WriteLine("             /   ,");
+
+           
+
+            Console.WriteLine("            /   '");
+            
+
+            Console.WriteLine("           /   ,'");
+            Console.WriteLine("           '.__|");
+            Console.WriteLine("            |  |");
+            Console.WriteLine("            |__|");
+            Console.WriteLine("            |  |");
+            Console.WriteLine("            |__|");
+            Console.WriteLine("            |  |");
+            Console.WriteLine("            |__|");
+            Console.WriteLine("            |  |");
+            Console.WriteLine("            |__|");
+            Console.WriteLine("            |, |");
+            Console.WriteLine("            |--|");
+            Console.WriteLine("            |__|");
+            Console.WriteLine("            |  |");
+            Console.WriteLine("            |--|");
+            Console.WriteLine("            |__|");
+            Console.WriteLine("            |__|        ,-.");
+            Console.WriteLine("            |__|'     ,'  /");
+            Console.WriteLine("       _,.-'     ',_,' o /");
+            Console.WriteLine("      /     8888        /");
+            Console.WriteLine("      |                /");
+            Console.WriteLine("       1              /");
+            Console.WriteLine("       `L   8888     /");
+            Console.WriteLine("        |           /");
+            Console.WriteLine("       /    ====    \\");
+            Console.WriteLine("      /     ____     \\");
+            Console.WriteLine("     /     (____)  o  \\");
+            Console.WriteLine("    /             o    \\");
+            Console.WriteLine("   /             o     ,'");
+            Console.WriteLine("  /               _,.'^");
+            Console.WriteLine(" /        __,.-\"~^");
+            Console.WriteLine("',,..--~~^");
+            Console.WriteLine("");
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
